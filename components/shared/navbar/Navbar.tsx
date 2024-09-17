8ba2299833fad0210fca664bcd4eddf106d38367
DjordjeJelenkovic <ledjo.jelenkovic@gmail.com>
2024-09-17 13:51:10 +0200

    Theme Switcher and Mobile Navigation(21)

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -2,13 +2,13 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-// import Theme from './Theme'
-// import MobileNav from './MobileNav'
-// import GlobalSearch from '../search/GlobalSearch'
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -22,10 +22,10 @@ const Navbar = () => {
         </p>
       </Link>
 
-      {/* <GlobalSearch /> */}
+      <GlobalSearch />
 
       <div className="flex-between gap-5">
-        {/* <Theme /> */}
+        <Theme />
 
         <SignedIn>
           <UserButton
@@ -41,7 +41,7 @@ const Navbar = () => {
           />
         </SignedIn>
 
-        {/* <MobileNav /> */}
+        <MobileNav />
       </div>
     </nav>
   );
